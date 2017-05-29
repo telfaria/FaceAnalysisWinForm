39bd6db0e6fd6cc8156c29afac171a589bf3fb09
Yuichi Yoshihara <tvb_elfaria@hotmail.co.jp>
2017-05-29 21:58:49 +0900

    Initial Commit

---
+++ FaceAnalysisWinForm/FaceAnalysisWinForm/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace FaceAnalysisWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                if (webCameraControl1.IsCapturing)
+                {
+                    webCameraControl1.StopCapture();
+                }
+
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webCameraControl1 = new WebEye.Controls.WinForms.WebCameraControl.WebCameraControl();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoVisionAPI = new System.Windows.Forms.RadioButton();
+            this.rdoEmotionAPI = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webCameraControl1
+            // 
+            this.webCameraControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webCameraControl1.Location = new System.Drawing.Point(12, 12);
+            this.webCameraControl1.Name = "webCameraControl1";
+            this.webCameraControl1.Size = new System.Drawing.Size(960, 540);
+            this.webCameraControl1.TabIndex = 0;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCapture.Location = new System.Drawing.Point(850, 564);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(154, 45);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "Capture!";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResults.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtResults.Location = new System.Drawing.Point(978, 12);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResults.Size = new System.Drawing.Size(334, 540);
+            this.txtResults.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.rdoVisionAPI);
+            this.groupBox1.Controls.Add(this.rdoEmotionAPI);
+            this.groupBox1.Location = new System.Drawing.Point(458, 558);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(386, 45);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select API";
+            // 
+            // rdoVisionAPI
+            // 
+            this.rdoVisionAPI.AutoSize = true;
+            this.rdoVisionAPI.Location = new System.Drawing.Point(236, 17);
+            this.rdoVisionAPI.Name = "rdoVisionAPI";
+            this.rdoVisionAPI.Size = new System.Drawing.Size(109, 22);
+            this.rdoVisionAPI.TabIndex = 1;
+            this.rdoVisionAPI.TabStop = true;
+            this.rdoVisionAPI.Text = "Vision API";
+            this.rdoVisionAPI.UseVisualStyleBackColor = true;
+            // 
+            // rdoEmotionAPI
+            // 
+            this.rdoEmotionAPI.AutoSize = true;
+            this.rdoEmotionAPI.Location = new System.Drawing.Point(86, 17);
+            this.rdoEmotionAPI.Name = "rdoEmotionAPI";
+            this.rdoEmotionAPI.Size = new System.Drawing.Size(124, 22);
+            this.rdoEmotionAPI.TabIndex = 0;
+            this.rdoEmotionAPI.TabStop = true;
+            this.rdoEmotionAPI.Text = "Emotion API";
+            this.rdoEmotionAPI.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1324, 615);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.webCameraControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private WebEye.Controls.WinForms.WebCameraControl.WebCameraControl webCameraControl1;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoVisionAPI;
+        private System.Windows.Forms.RadioButton rdoEmotionAPI;
+    }
+}
+
---
+++ FaceAnalysisWinForm/FaceAnalysisWinForm/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebEye.Controls.WinForms.WebCameraControl;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using System.Configuration;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+
+
+
+namespace FaceAnalysisWinForm
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap capturedImage;
+
+        const string EmotionAPIKEY = "YOUR EMOTION API KEYS HERE";
+        const string VisionAPIKEY = "YOUR VISION API KEY HERE";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            List<WebCameraId> cameras = new List<WebCameraId>(webCameraControl1.GetVideoCaptureDevices());
+
+            webCameraControl1.StartCapture(cameras[0]);
+
+            rdoVisionAPI.Checked = true;
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            //ボタンを連続押しできないようにする
+            btnCapture.Enabled = false;
+            //カメラからのキャプチャをする
+            capturedImage = webCameraControl1.GetCurrentImage();
+
+            ////いったん取得を止める
+            //if (webCameraControl1.IsCapturing)
+            //{
+            //    webCameraControl1.StopCapture();
+            //}
+
+            //取得したImageを一回保存する
+            string path = Application.StartupPath;
+            string imgFileName = System.IO.Path.Combine(path, string.Format("captured_{0:yyyyMMddhhmmss}.png", DateTime.Now));
+            capturedImage.Save(imgFileName);
+
+            txtResults.Clear();
+
+            //解析をする
+            if (rdoEmotionAPI.Checked)
+            ImageAnalysisEmotion(imgFileName);
+
+            if(rdoVisionAPI.Checked)
+            ImageAnalysisVision(imgFileName);
+
+            ////解析が終わったら、StartCaptureする
+            //if (!webCameraControl1.IsCapturing)
+            //{
+            //    webCameraControl1.StartCapture(camera[0]);
+            //}
+
+            //ボタンを有効状態に戻す
+            btnCapture.Enabled = true;
+
+        }
+
+        private async void ImageAnalysisVision(string imgFileName)
+        {
+
+            var vclient = new VisionServiceClient(VisionAPIKEY);
+            AnalysisResult aResult = null;
+
+            try
+            {
+                VisualFeature[] vFeature = new VisualFeature[] {
+                    VisualFeature.Adult,
+                    VisualFeature.Description,
+                    VisualFeature.Faces ,
+                    VisualFeature.Categories,
+                    VisualFeature.Color,
+                    VisualFeature.ImageType,
+                    VisualFeature.Tags
+                };
+                txtResults.AppendText("Vision API 解析中...\r\n");
+
+                aResult = await vclient.AnalyzeImageAsync(new System.IO.FileStream(imgFileName, System.IO.FileMode.Open), vFeature);
+            }
+            catch(Exception e)
+            {
+                txtResults.AppendText("検出できませんでした\r\n");
+            }
+
+            string resultText = "";
+
+            if (aResult != null)
+            {
+                resultText += "Adult Score: " + Math.Round(aResult.Adult.AdultScore *100,2) + "%"  + "\r\n";
+                resultText += "Is adult content?: " + aResult.Adult.IsAdultContent + "\r\n";
+
+                string caps = "";
+                foreach (var captions in aResult.Description.Captions)
+                {
+                    caps += Math.Round(captions.Confidence * 100,2)+ "%" + " : " + captions.Text + "\r\n";
+
+                }
+
+                resultText += "Description: \r\n" + caps;
+
+                txtResults.AppendText("Vision APIの解析結果：\r\n");
+                txtResults.AppendText(resultText);
+
+            }
+            else
+            {
+                txtResults.AppendText("Vision APIの呼び出しに失敗しました");
+
+            }
+
+        }
+
+        private async void ImageAnalysisEmotion(string ImgFileName)
+        {
+
+
+            var res = "";
+
+            var eClient = new EmotionServiceClient(EmotionAPIKEY);
+            Emotion[] eResult = null;
+
+            try
+            {
+                txtResults.AppendText("Emotion API 解析中...\r\n");
+                eResult = await eClient.RecognizeAsync(new System.IO.FileStream(ImgFileName, System.IO.FileMode.Open));
+
+            }
+            catch (Exception e)
+            {
+                eResult = null;
+                res = "検出できませんでした";
+                txtResults.AppendText(res);
+
+                return;
+            }
+
+            if (eResult != null)
+            {   //データの整形と出力
+                res = "Emotion API 解析できました";
+                string resultText = "";
+                for (int i = 0; i <= eResult.Length - 1; i++)
+                {
+                    var eScores = eResult[i].Scores;
+                    // 検出された表情スコアをセット
+                    resultText += string.Format("要素 {0} 個め\r\n", i);
+                    resultText += string.Format("怒り: {0}%\r\n", Math.Round(eScores.Anger * 100, 2));
+                    resultText += string.Format("軽蔑:{0}%\r\n", Math.Round(eScores.Contempt * 100, 2));
+                    resultText += string.Format("ムカつき:{0}%\r\n", Math.Round(eScores.Disgust * 100, 2));
+                    resultText += string.Format("恐れ:{0}%\r\n", Math.Round(eScores.Fear * 100, 2));
+                    resultText += string.Format("喜び:{0}%\r\n", Math.Round(eScores.Happiness * 100, 2));
+                    resultText += string.Format("無表情:{0}%\r\n", Math.Round(eScores.Neutral * 100, 2));
+                    resultText += string.Format("悲しみ:{0}%\r\n", Math.Round(eScores.Sadness * 100, 2));
+                    resultText += string.Format("驚き:{0}%\r\n", Math.Round(eScores.Surprise * 100, 2));
+                }
+                txtResults.AppendText(string.Format("{0}\r\n{1}", res, resultText));
+            }
+            else
+            {
+                res = "API呼び出しに失敗しました";
+                txtResults.AppendText(res);
+
+            }
+
+        }
+
+    }
+
+
+
+
+}
